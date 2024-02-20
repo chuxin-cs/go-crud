c5e553362aa4d9e5b0796bc2f9cd5e8d7731fe3f
chuxin <15115359055@139.com>
2024-02-20 08:50:36 +0800

    feat: 注释掉<React.StrictMode>

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
+
+function useBoolean(value: any) {
+  const [state, setState] = useState(value);
+  const actions = useMemo(() => {
+    console.log(111);
+    return {
+      toggle: () => {
+        setState(!state);
+      },
+      set: (v: any) => {
+        setState(v);
+      },
+      setTrue: () => {
+        setState(true);
+      },
+      setFalse: () => {
+        setState(false);
+      },
+    };
+  }, []);
+
+  return [state, actions];
+}
 
 function App() {
   const [count, setCount] = useState(0);
+  const [state] = useBoolean(true);
+  const [state1] = useBoolean(true);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          1
-        </a>
-        <a href="https://react.dev" target="_blank">
-          2
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>
+        Vite + React {state} = {state1}
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
